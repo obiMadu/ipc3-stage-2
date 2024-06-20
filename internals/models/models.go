8f530bf8b7db8271ef9fa893e14e32eb5694f2bc
Obi.M <mail@obimadu.pro>
2024-06-20 16:37:21 +0100

    fix: update type json tag

--- internals/models/models.go
+++ internals/models/models.go
@@ -4,5 +4,5 @@ type Users struct {
 	ID       uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 	Username string `json:"username" gorm:"unique;not null"`
 	Email    string `json:"email" gorm:"unique;not null"`
-	Fullname string `json:"fullname"`
+	Fullname string `json:"fullname,omitempty"`
 }
