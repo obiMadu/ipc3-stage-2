5f9bc5b62e40c13ada1e7d5c67ee9a7466729c66
Obi.M <mail@obimadu.pro>
2024-06-20 16:36:49 +0100

    feat: create http routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func router() *gin.Engine {
+	// make router
+	mux := gin.Default()
+
+	// use cors
+	mux.Use(cors.Default())
+
+	// routes
+	// routes.users
+	mux.POST("/users", createUser)
+
+	mux.GET("/users", getUser)
+	mux.GET("/users/:userID", getUser)
+
+	mux.PUT("/users", updateUser)
+	mux.PUT("users/:userID", updateUser)
+
+	mux.DELETE("users", deleteUser)
+	mux.DELETE("users/:userID", deleteUser)
+
+	return mux
+}
