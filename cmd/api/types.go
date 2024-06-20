ad4e9048b4d263b63d5b15deb54b96114c1bcad7
Obi.M <mail@obimadu.pro>
2024-06-20 04:30:29 +0100

    feat: create http request and response types

--- cmd/api/types.go
+++ cmd/api/types.go
@@ -1,1 +1,14 @@
 package main
+
+type jsonRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Fullname string `json:"fullname,omitempty"`
+}
+
+type jsonResponse struct {
+	Status  string         `json:"status" binding:"required"`
+	Message string         `json:"message" binding:"required"`
+	Data    map[string]any `json:"data" binding:"required"`
+	Error   map[string]any `json:"error,omitempty"`
+}
