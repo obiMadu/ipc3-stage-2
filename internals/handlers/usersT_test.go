efb52887dfa31737ec3bb4c1e227f4cb1f24d484
Obi.M <mail@obimadu.pro>
2024-06-23 04:59:07 +0100

    test: work on users handlers tests

--- internals/handlers/usersT_test.go
+++ internals/handlers/usersT_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -15,12 +16,15 @@ import (
 )
 
 type args struct {
-	sqlStatement      string
-	returnRows        [][]any
-	httpRequestURL    string
-	httpRequestMethod string
-	httpCode          int
-	httpResponse      jsonResponse
+	sqlStatus            string
+	sqlStatement         string
+	sqlErrorText         string
+	sqlReturnRows        [][]any
+	httpRequestURL       string
+	httpRequestMethod    string
+	httpRequestBody      map[string]any
+	httpResponseBodyCode int
+	httpResponseBody     jsonResponse
 }
 
 type test struct {
@@ -46,6 +50,137 @@ func InitDB(t *testing.T) (*gorm.DB, sqlmock.Sqlmock) {
 	return db, mock
 }
 
+func TestCreateUser(t *testing.T) {
+	db, mock := InitDB(t)
+
+	tests := []test{
+		{
+			name: "Create user accurately",
+			args: args{
+				sqlStatement:      ".*",
+				httpRequestURL:    "/",
+				httpRequestMethod: "POST",
+				httpRequestBody: gin.H{
+					"username": "user1",
+					"email":    "user1@example.com",
+				},
+				httpResponseBodyCode: http.StatusOK,
+				httpResponseBody: jsonResponse{
+					Status:  "success",
+					Message: "User created successfully.",
+				},
+			},
+		},
+		{
+			name: "Create user without username",
+			args: args{
+				sqlStatement:      ".*",
+				httpRequestURL:    "/",
+				httpRequestMethod: "POST",
+				httpRequestBody: gin.H{
+					"email": "user1@example.com",
+				},
+				httpResponseBodyCode: http.StatusBadRequest,
+				httpResponseBody: jsonResponse{
+					Status:  "error",
+					Message: "You must specify both a username and an email.",
+				},
+			},
+		},
+		{
+			name: "Create user without email",
+			args: args{
+				sqlStatement:      ".*",
+				httpRequestURL:    "/",
+				httpRequestMethod: "POST",
+				httpRequestBody: gin.H{
+					"username": "user1",
+				},
+				httpResponseBodyCode: http.StatusBadRequest,
+				httpResponseBody: jsonResponse{
+					Status:  "error",
+					Message: "You must specify both a username and an email.",
+				},
+			},
+		},
+		// {
+		// 	name: "Create user with duplicate username",
+		// 	args: args{
+		// 		sqlStatement:      ".*",
+		// 		sqlStatus:         "error",
+		// 		sqlErrorText:      "duplicate key value violates unique constraint",
+		// 		httpRequestURL:    "/",
+		// 		httpRequestMethod: "POST",
+		// 		httpRequestBody: gin.H{
+		// 			"username": "user2",
+		// 			"email":    "user2@example.com",
+		// 		},
+		// 		httpResponseBodyCode: http.StatusBadRequest,
+		// 		httpResponseBody: jsonResponse{
+		// 			Status:  "error",
+		// 			Message: "Username has been taken!",
+		// 		},
+		// 	},
+		// },
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			mock.ExpectBegin()
+			mock.ExpectQuery(".*").WillReturnRows(sqlmock.NewRows(nil))
+
+			// switch test.args.sqlStatus {
+			// case "error":
+			// 	mock.ExpectQuery(".*").WillReturnError(errors.New((test.args.sqlErrorText)))
+			// default:
+			// 	mock.ExpectQuery(".*").WillReturnRows(sqlmock.NewRows(nil))
+			// }
+
+			mock.ExpectCommit()
+
+			gin.SetMode(gin.ReleaseMode)
+			r := gin.New()
+			r.Use(gin.Logger())
+			r.POST("/", func(c *gin.Context) {
+				CreateUser(c, db)
+			})
+
+			// Parse the URL to ensure query parameters are correctly included
+			parsedURL, err := url.Parse(test.args.httpRequestURL)
+			if err != nil {
+				t.Fatalf("Failed to parse request URL %v", err)
+			}
+
+			requestBody, err := json.Marshal(test.args.httpRequestBody)
+			if err != nil {
+				t.Fatalf("Unable to marshal request body %v", err)
+			}
+
+			req, err := http.NewRequest(test.args.httpRequestMethod, parsedURL.String(), bytes.NewReader(requestBody))
+			if err != nil {
+				t.Fatalf("Failed to create request %v", err)
+			}
+
+			if requestBody != nil {
+				req.Header.Set("Content-Type", "application/json")
+			}
+
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+
+			var responseBody jsonResponse
+			err = json.Unmarshal(w.Body.Bytes(), &responseBody)
+			if err != nil {
+				t.Fatalf("Failed to Unmarshall response body: %v", err)
+			}
+
+			assert.Equal(t, test.args.httpResponseBodyCode, w.Code)
+			assert.Equal(t, test.args.httpResponseBody, responseBody)
+		})
+	}
+}
+
 func TestGetAll(t *testing.T) {
 	db, mock := InitDB(t)
 
@@ -54,14 +189,14 @@ func TestGetAll(t *testing.T) {
 			name: "Get all 2 rows",
 			args: args{
 				sqlStatement: ".*",
-				returnRows: [][]any{
+				sqlReturnRows: [][]any{
 					{1, "Obi", "obi@example.com"},
 					{2, "Marry", "marry@example.com"},
 				},
-				httpRequestURL:    "/",
-				httpRequestMethod: "GET",
-				httpCode:          http.StatusOK,
-				httpResponse: jsonResponse{
+				httpRequestURL:       "/",
+				httpRequestMethod:    "GET",
+				httpResponseBodyCode: http.StatusOK,
+				httpResponseBody: jsonResponse{
 					Status:  "success",
 					Message: "Retrieved all users.",
 					Data: gin.H{
@@ -77,13 +212,13 @@ func TestGetAll(t *testing.T) {
 			name: "Get user by username",
 			args: args{
 				sqlStatement: ".*",
-				returnRows: [][]any{
+				sqlReturnRows: [][]any{
 					{1, "Obi", "obi@example.com"},
 				},
-				httpRequestURL:    "/?username=Obi",
-				httpRequestMethod: "GET",
-				httpCode:          http.StatusOK,
-				httpResponse: jsonResponse{
+				httpRequestURL:       "/?username=Obi",
+				httpRequestMethod:    "GET",
+				httpResponseBodyCode: http.StatusOK,
+				httpResponseBody: jsonResponse{
 					Status:  "success",
 					Message: "User retrieved successfully.",
 					Data: gin.H{
@@ -97,7 +232,7 @@ func TestGetAll(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			rows := sqlmock.NewRows([]string{"id", "username", "email"})
-			for _, row := range test.args.returnRows {
+			for _, row := range test.args.sqlReturnRows {
 				rows.AddRow(row[0], row[1], row[2])
 			}
 
@@ -130,8 +265,8 @@ func TestGetAll(t *testing.T) {
 				t.Fatalf("Failed to Unmarshall response body: %v", err)
 			}
 
-			assert.Equal(t, test.args.httpCode, w.Code)
-			assert.Equal(t, test.args.httpResponse, responseBody)
+			assert.Equal(t, test.args.httpResponseBodyCode, w.Code)
+			assert.Equal(t, test.args.httpResponseBody, responseBody)
 		})
 	}
 }
@@ -144,13 +279,13 @@ func TestGetUserByID(t *testing.T) {
 			name: "Get user by ID",
 			args: args{
 				sqlStatement: ".*",
-				returnRows: [][]any{
+				sqlReturnRows: [][]any{
 					{1, "Obi", "obi@example.com"},
 				},
-				httpRequestURL:    "/1",
-				httpRequestMethod: "GET",
-				httpCode:          http.StatusOK,
-				httpResponse: jsonResponse{
+				httpRequestURL:       "/1",
+				httpRequestMethod:    "GET",
+				httpResponseBodyCode: http.StatusOK,
+				httpResponseBody: jsonResponse{
 					Status:  "success",
 					Message: "User retrieved successfully.",
 					Data: gin.H{
@@ -163,13 +298,13 @@ func TestGetUserByID(t *testing.T) {
 			name: "Get user by string ID",
 			args: args{
 				sqlStatement: ".*",
-				returnRows: [][]any{
+				sqlReturnRows: [][]any{
 					{1, "Obi", "obi@example.com"},
 				},
-				httpRequestURL:    "/one",
-				httpRequestMethod: "GET",
-				httpCode:          http.StatusBadRequest,
-				httpResponse: jsonResponse{
+				httpRequestURL:       "/one",
+				httpRequestMethod:    "GET",
+				httpResponseBodyCode: http.StatusBadRequest,
+				httpResponseBody: jsonResponse{
 					Status:  "error",
 					Message: "UserID must be a positive interger.",
 					Data:    nil,
@@ -181,7 +316,7 @@ func TestGetUserByID(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			rows := sqlmock.NewRows([]string{"id", "username", "email"})
-			for _, row := range test.args.returnRows {
+			for _, row := range test.args.sqlReturnRows {
 				rows.AddRow(row[0], row[1], row[2])
 			}
 
@@ -214,8 +349,202 @@ func TestGetUserByID(t *testing.T) {
 				t.Fatalf("Failed to Unmarshall response body: %v", err)
 			}
 
-			assert.Equal(t, test.args.httpCode, w.Code)
-			assert.Equal(t, test.args.httpResponse, responseBody)
+			assert.Equal(t, test.args.httpResponseBodyCode, w.Code)
+			assert.Equal(t, test.args.httpResponseBody, responseBody)
+		})
+	}
+}
+
+func TestDeleteUserByID(t *testing.T) {
+	db, mock := InitDB(t)
+
+	tests := []test{
+		{
+			name: "Delete user by ID",
+			args: args{
+				sqlStatement: ".*",
+				sqlReturnRows: [][]any{
+					{1, "Obi", "obi@example.com"},
+				},
+				httpRequestURL:       "/1",
+				httpRequestMethod:    "DELETE",
+				httpResponseBodyCode: http.StatusOK,
+				httpResponseBody: jsonResponse{
+					Status:  "success",
+					Message: "User deleted successfully.",
+					Data:    nil,
+				},
+			},
+		},
+		{
+			name: "Delete user by non-existent ID",
+			args: args{
+				sqlStatement:         ".*",
+				sqlReturnRows:        [][]any{},
+				httpRequestURL:       "/5",
+				httpRequestMethod:    "DELETE",
+				httpResponseBodyCode: http.StatusBadRequest,
+				httpResponseBody: jsonResponse{
+					Status:  "error",
+					Message: "User does not exist.",
+					Data:    nil,
+				},
+			},
+		},
+		{
+			name: "Delete user by string ID",
+			args: args{
+				sqlStatement:         ".*",
+				httpRequestURL:       "/one",
+				httpRequestMethod:    "DELETE",
+				httpResponseBodyCode: http.StatusBadRequest,
+				httpResponseBody: jsonResponse{
+					Status:  "error",
+					Message: "UserID must be a positive interger.",
+					Data:    nil,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			rows := sqlmock.NewRows([]string{"id", "username", "email"})
+			for _, row := range test.args.sqlReturnRows {
+				rows.AddRow(row[0], row[1], row[2])
+			}
+
+			mock.ExpectQuery(test.args.sqlStatement).WillReturnRows(rows)
+			mock.ExpectBegin()
+			mock.ExpectExec(test.args.sqlStatement).WillReturnResult(sqlmock.NewResult(1, 1))
+			mock.ExpectCommit()
+
+			gin.SetMode(gin.ReleaseMode)
+			r := gin.New()
+			r.Use(gin.Logger())
+			r.DELETE("/:userID", func(c *gin.Context) {
+				DeleteUserByID(c, db)
+			})
+
+			// Parse the URL to ensure query parameters are correctly included
+			parsedURL, err := url.Parse(test.args.httpRequestURL)
+			if err != nil {
+				t.Fatalf("Failed to parse request URL %v", err)
+			}
+
+			req, err := http.NewRequest(test.args.httpRequestMethod, parsedURL.String(), nil)
+			if err != nil {
+				t.Fatalf("Failed to create request %v", err)
+			}
+
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+
+			var responseBody jsonResponse
+			err = json.Unmarshal(w.Body.Bytes(), &responseBody)
+			if err != nil {
+				t.Fatalf("Failed to Unmarshall response body: %v", err)
+			}
+
+			assert.Equal(t, test.args.httpResponseBodyCode, w.Code)
+			assert.Equal(t, test.args.httpResponseBody, responseBody)
+		})
+	}
+}
+
+func TestDeleteUserByUsername(t *testing.T) {
+	db, mock := InitDB(t)
+
+	tests := []test{
+		{
+			name: "Delete user by Username",
+			args: args{
+				sqlStatement: ".*",
+				sqlReturnRows: [][]any{
+					{1, "Obi", "obi@example.com"},
+				},
+				httpRequestURL:       "/?username=Obi",
+				httpRequestMethod:    "DELETE",
+				httpResponseBodyCode: http.StatusOK,
+				httpResponseBody: jsonResponse{
+					Status:  "success",
+					Message: "User deleted successfully.",
+				},
+			},
+		},
+		{
+			name: "Delete non-existent user by username",
+			args: args{
+				sqlStatement:         ".*",
+				sqlReturnRows:        [][]any{},
+				httpRequestURL:       "/?username=two",
+				httpRequestMethod:    "DELETE",
+				httpResponseBodyCode: http.StatusBadRequest,
+				httpResponseBody: jsonResponse{
+					Status:  "error",
+					Message: "User does not exist.",
+				},
+			},
+		},
+		{
+			name: "Call delete without username query",
+			args: args{
+				sqlStatement:         ".*",
+				sqlReturnRows:        [][]any{},
+				httpRequestURL:       "/",
+				httpRequestMethod:    "DELETE",
+				httpResponseBodyCode: http.StatusBadRequest,
+				httpResponseBody: jsonResponse{
+					Status:  "error",
+					Message: "You must/can only specify a user to delete.",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			rows := sqlmock.NewRows([]string{"id", "username", "email"})
+			for _, row := range test.args.sqlReturnRows {
+				rows.AddRow(row[0], row[1], row[2])
+			}
+
+			mock.ExpectQuery(test.args.sqlStatement).WillReturnRows(rows)
+			mock.ExpectBegin()
+			mock.ExpectExec(test.args.sqlStatement).WithArgs(sqlmock.AnyArg()).WillReturnResult(sqlmock.NewResult(0, 0))
+			mock.ExpectCommit()
+
+			gin.SetMode(gin.ReleaseMode)
+			r := gin.New()
+			r.Use(gin.Logger())
+			r.DELETE("/", func(c *gin.Context) {
+				DeleteUserByUsername(c, db)
+			})
+
+			// Parse the URL to ensure query parameters are correctly included
+			parsedURL, err := url.Parse(test.args.httpRequestURL)
+			if err != nil {
+				t.Fatalf("Failed to parse request URL %v", err)
+			}
+
+			req, err := http.NewRequest(test.args.httpRequestMethod, parsedURL.String(), nil)
+			if err != nil {
+				t.Fatalf("Failed to create request %v", err)
+			}
+
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+
+			var responseBody jsonResponse
+			err = json.Unmarshal(w.Body.Bytes(), &responseBody)
+			if err != nil {
+				t.Fatalf("Failed to Unmarshall response body: %v", err)
+			}
+
+			assert.Equal(t, test.args.httpResponseBodyCode, w.Code)
+			assert.Equal(t, test.args.httpResponseBody, responseBody)
 		})
 	}
 }
